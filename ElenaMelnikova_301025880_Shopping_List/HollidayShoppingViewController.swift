739abd9e75c7c8769af3bcda5f87988cc2737f1d
Elena Melnikova <elenamelnikova@owners-macbook-pro.local>
2018-10-18 19:28:41 -0400

    TabBarViewController Scene added; Conputer Parts View created

---
+++ ElenaMelnikova_301025880_Shopping_List/HollidayShoppingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HollidayShoppingViewController.swift
+//  ElenaMelnikova_301025880_Shopping_List
+//
+//  Created by Elena Melnikova on 2018-10-18.
+//  Copyright © 2018 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class HollidayShoppingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
