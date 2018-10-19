43f49bfeef3516b919cc57a9ea929d77bd6246af
Elena Melnikova <elenamelnikova@owners-macbook-pro.local>
2018-10-18 20:53:28 -0400

    Picker for Conputer Parts View Controller Scene filed

--- ElenaMelnikova_301025880_Shopping_List/ConputerPartsViewController.swift
+++ ElenaMelnikova_301025880_Shopping_List/ConputerPartsViewController.swift
@@ -8,8 +8,20 @@
 
 import UIKit
 
-class ConputerPartsViewController: UIViewController {
+class ConputerPartsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var computerPartsPicker: UIPickerView!
+    private let computerPartsComponent = 0
+    private let computerPartsQuantityComponent = 1
+    
+    private let characterComputerParts = [
+        "Memory", "CPU", "Motherboards", "Fans", "Cases"]
+    
+    
+    private let characterComputerPartsQuantity = [
+        "1", "2", "3", "4", "5"]
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,5 +43,38 @@ class ConputerPartsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    @IBAction func savecomputerParts(_ sender: UIButton) {
+        let row_computerParts = computerPartsPicker.selectedRow(inComponent: 0)
+        let row_computerPartsQuantity = computerPartsPicker.selectedRow(inComponent: 1)
+        
+        let selected_computerParts =
+            characterComputerParts[row_computerParts]
+        let selected_computerPartsQuantity =
+            characterComputerPartsQuantity[row_computerPartsQuantity]
+        
+        
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    func pickerView(_ pickerView: UIPickerView,
+                    numberOfRowsInComponent component: Int) -> Int {
+        if component == computerPartsComponent{
+            return characterComputerParts.count
+        } else {
+            return characterComputerPartsQuantity.count
+        }
+    }
+    
+    // MARK: Picker Delegate Methods
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == computerPartsComponent {
+            return characterComputerParts[row]
+        } else {
+            return characterComputerPartsQuantity[row]
+        }
+    }
+    
 }
+
+
